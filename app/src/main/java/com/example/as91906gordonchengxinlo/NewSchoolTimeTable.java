81b13023491d4755ee41b3970d8cd52ccf8f29df
gordon <lochengxin@gmail.com>
2021-05-20 00:23:03 +1200

    Set pop up window for new scholltimetable by onclick set the cross function for it

--- app/src/main/java/com/example/as91906gordonchengxinlo/NewSchoolTimeTable.java
+++ app/src/main/java/com/example/as91906gordonchengxinlo/NewSchoolTimeTable.java
@@ -16,7 +16,39 @@ public class NewSchoolTimeTable extends AppCompatActivity {
         setContentView(R.layout.activity_new_school_time_table);
     }
 
+    public void Save_change(View aView) {
+        /*
+         * up data the setting by the get value and update to the list by showing the text view
+         * */
 
+        /*Edit.putExtra()*/
+        TextView EditedClass = (TextView)findViewById(R.id.SchoolTimetable_New_Class);
+        TextView EditedDay = (TextView)findViewById(R.id.SchoolTimetable_New_Day);
+        TextView EditedLocation = (TextView)findViewById(R.id.SchoolTimetable_New_Location);
+        TextView EditedStartTime = (TextView)findViewById(R.id.SchoolTimetable_New_Starting_time);
+        TextView EditedEndTime = (TextView)findViewById(R.id.SchoolTimetable_New_Ending_time);
+
+        CharSequence Class =EditedClass.getText();
+        CharSequence Day =EditedDay.getText();
+        CharSequence Location =EditedLocation.getText();
+        CharSequence StartTime =EditedStartTime.getText();
+        CharSequence EndTime =EditedEndTime.getText();
+                /*
+                up date the list in teh view class
+
+                */
+        AlertDialog.Builder Check = new AlertDialog.Builder(NewSchoolTimeTable.this);
+        Check.setMessage("Please make sure the value is correct below"+"\n\nClass==>  "+Class+"\n\nClass Day==>  "+Day+"\n\nLocation==>  "+Location+"\n\nStart At==>  "+StartTime+"\n\nEnd In==>  "+EndTime);
+
+        Check.setPositiveButton("Save and Leave", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                finish();
+                /*update function to teh list*/
+            }
+        });
+        Check.show();
+    }
 
     public void Leave(View aView) {
         AlertDialog.Builder Warning = new AlertDialog.Builder(NewSchoolTimeTable.this);
