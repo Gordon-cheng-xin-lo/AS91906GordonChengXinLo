1fd18c9225f29082444047dbc370f2ab4684a09e
gordon <lochengxin@gmail.com>
2021-05-19 23:13:06 +1200

    Set Sub all 4 function page and create the function cross between each

---
+++ app/src/main/java/com/example/as91906gordonchengxinlo/Revision.java
@@ -0,0 +1,20 @@
+package com.example.as91906gordonchengxinlo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Revision extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_revision);
+
+    }
+}
